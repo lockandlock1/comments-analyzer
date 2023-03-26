e6b0cbd3d0a2f64cf6eae256df3cd73d1becc20b
redkyu <hongstar2311@gmail.com>
2023-03-26 20:02:23 +0900

    fix: schools service repository 역할 분리 , stream 적용

--- src/main/java/com/kakaobank/repository/SchoolRepository.java
+++ src/main/java/com/kakaobank/repository/SchoolRepository.java
@@ -18,108 +18,111 @@ public class SchoolRepository {
         this.db = db;
     }
 
-    public String findSchool(SchoolToken schoolToken) {
-        List<Pattern> matchedPatterns = new ArrayList<>();
+    public List<School> findBySchoolNameAndLevel(String schoolName, String levelPattern) {
 
-        List<Pattern> unmatchedPatterns = new ArrayList<>();
-
-        String levelPattern = AnalyzerUtils.getPatternFromLevel(schoolToken.getLevel());
-        List<String> kindPatterns = AnalyzerUtils.getPatternFromKinds(schoolToken.getKinds());
-
-        if (kindPatterns.size() > 0) {
-            matchedPatterns.add(Pattern.compile(String.format("(%s)+.*%s$",
-                    Pattern.quote(String.join("|", kindPatterns)),
-                    Pattern.quote(levelPattern))));
-        }
-
-
-        List<School> schools = calculateSchoolStatistics(db.selectSchoolList(schoolToken.getSchoolName(), levelPattern), schoolToken, matchedPatterns, unmatchedPatterns);
-
-
-
-        if (schools.size() == 0) {
-            return null;
-        }
-
-        schools.sort(new Comparator<School>() {
-            @Override
-            public int compare(School o1, School o2) {
-                Integer o1Value = o1.getScore();
-                Integer o2Value = o2.getScore();
-                Integer o1Len = o1.getName().length();
-                Integer o2Len = o2.getName().length();
-                if (o1Value.equals(o2Value)) {
-                    return o1Len.compareTo(o2Len);
-                } else {
-                    return o2Value.compareTo(o1Value);
-                }
-            }
-        });
-
-
-
-        return schools.get(0).getName();
+        return new ArrayList<>(db.selectSchoolList(schoolName, levelPattern));
     }
 
-
-    private List<School> calculateSchoolStatistics(List<School> schools, SchoolToken schoolToken, List<Pattern> matchedPatterns, List<Pattern> unmatchedPatterns) {
-        List<School> list = new ArrayList<>();
-        for (School school : schools) {
-            calculateScore(schoolToken, school);
-
-            int matched = patternFindFromSchoolName(school.getName(), matchedPatterns);
-            int unmatched = patternFindFromSchoolName(school.getName(), unmatchedPatterns);
-
-            if (matched != matchedPatterns.size() || unmatched > 0) continue;
-
-
-            list.add(school);
-        }
-
-        return new ArrayList<>(list);
-    }
-
-    private void calculateScore(SchoolToken schoolToken, School school) {
-        if (schoolToken.isGirlKind() && school.getGender().equals("녀")) {
-            school.plusScore();
-        }
-
-        if (!schoolToken.isGirlKind() && !school.getGender().equals("녀")) {
-            school.plusScore();
-        }
-
-
-        if (schoolToken.isSpecialKind() && Arrays.asList("특수목적고등학교", "특성화고등학교").contains(school.getProperty())) {
-            school.plusScore();
-        }
-
-        if (schoolToken.hasLocationIn(school.getName())) {
-            school.plusScore();;
-        }
-
-        if (schoolToken.hasLocationIn(school.getLocation())) {
-            school.plusScore();
-        }
-
-    }
-
-    private int patternFindFromSchoolName(String schoolName, List<Pattern> patterns) {
-        if (patterns.size() == 0) {
-            return 0;
-        }
-
-        int count = 0;
-
-        for (Pattern pattern : patterns) {
-            if (isMatch(pattern.matcher(schoolName))) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private boolean isMatch(Matcher matcher) {
-        return matcher.find();
-    }
+//    public String findSchool(SchoolToken schoolToken) {
+//        List<Pattern> matchedPatterns = new ArrayList<>();
+//
+//
+//        String levelPattern = AnalyzerUtils.getPatternFromLevel(schoolToken.getLevel());
+//        List<String> kindPatterns = AnalyzerUtils.getPatternFromKinds(schoolToken.getKinds());
+//
+//        if (kindPatterns.size() > 0) {
+//            matchedPatterns.add(Pattern.compile(String.format("(%s)+.*%s$",
+//                    Pattern.quote(String.join("|", kindPatterns)),
+//                    Pattern.quote(levelPattern))));
+//        }
+//
+//
+//        List<School> schools = calculateSchoolStatistics(db.selectSchoolList(schoolToken.getSchoolName(), levelPattern), schoolToken, matchedPatterns);
+//
+//
+//
+//        if (schools.size() == 0) {
+//            return null;
+//        }
+//
+//        schools.sort(new Comparator<School>() {
+//            @Override
+//            public int compare(School o1, School o2) {
+//                Integer o1Value = o1.getScore();
+//                Integer o2Value = o2.getScore();
+//                Integer o1Len = o1.getName().length();
+//                Integer o2Len = o2.getName().length();
+//                if (o1Value.equals(o2Value)) {
+//                    return o1Len.compareTo(o2Len);
+//                } else {
+//                    return o2Value.compareTo(o1Value);
+//                }
+//            }
+//        });
+//
+//
+//
+//        return schools.get(0).getName();
+//    }
+//
+//
+//    private List<School> calculateSchoolStatistics(List<School> schools, SchoolToken schoolToken, List<Pattern> matchedPatterns) {
+//        List<School> list = new ArrayList<>();
+//        for (School school : schools) {
+//            calculateScore(schoolToken, school);
+//
+//            int matched = patternFindFromSchoolName(school.getName(), matchedPatterns);
+//
+//            if (matched != matchedPatterns.size()) continue;
+//
+//
+//            list.add(school);
+//        }
+//
+//        return new ArrayList<>(list);
+//    }
+//
+//    private void calculateScore(SchoolToken schoolToken, School school) {
+//        if (schoolToken.isGirlKind() && school.getGender().equals("녀")) {
+//            school.plusScore();
+//        }
+//
+//        if (!schoolToken.isGirlKind() && !school.getGender().equals("녀")) {
+//            school.plusScore();
+//        }
+//
+//
+//        if (schoolToken.isSpecialKind() && Arrays.asList("특수목적고등학교", "특성화고등학교").contains(school.getProperty())) {
+//            school.plusScore();
+//        }
+//
+//        if (schoolToken.hasLocationIn(school.getName())) {
+//            school.plusScore();;
+//        }
+//
+//        if (schoolToken.hasLocationIn(school.getLocation())) {
+//            school.plusScore();
+//        }
+//
+//    }
+//
+//    private int patternFindFromSchoolName(String schoolName, List<Pattern> patterns) {
+//        if (patterns.size() == 0) {
+//            return 0;
+//        }
+//
+//        int count = 0;
+//
+//        for (Pattern pattern : patterns) {
+//            if (isMatch(pattern.matcher(schoolName))) {
+//                count++;
+//            }
+//        }
+//
+//        return count;
+//    }
+//
+//    private boolean isMatch(Matcher matcher) {
+//        return matcher.find();
+//    }
 }
--- src/main/java/com/kakaobank/service/CommentsAnalyzerService.java
+++ src/main/java/com/kakaobank/service/CommentsAnalyzerService.java
@@ -1,5 +1,6 @@
 package com.kakaobank.service;
 
+import com.kakaobank.domain.school.School;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.kakaobank.repository.db.DQLService;
@@ -21,10 +22,10 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static com.kakaobank.domain.school.SchoolTypes.*;
 
@@ -73,6 +74,55 @@ public class CommentsAnalyzerService {
         return new HashMap<>(schoolStatusBoard);
     }
 
+//    private List<String> parse(String text) {
+//        List<String> locations = AnalyzerUtils.getLocationsFromText(text);
+//
+//        // Normalize
+//        CharSequence normalized = OpenKoreanTextProcessorJava.normalize(text);
+//
+//        // Tokenize
+//        Seq<KoreanTokenizer.KoreanToken> tokens = OpenKoreanTextProcessorJava.tokenize(normalized);
+//        List<KoreanTokenJava> filteredKoreaTokens = AnalyzerUtils.filterPos(OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(tokens));
+//
+//        List<String> words = new ArrayList<>();
+//
+//        for (KoreanTokenJava koreaToken : filteredKoreaTokens) {
+//            words.add(AnalyzerUtils.removeNonKorean(koreaToken.getText()));
+//        }
+//
+//        int usedIndex = 0;
+//        List<String> schools = new ArrayList<>();
+//        for (int currentIndex = 0; currentIndex < words.size(); currentIndex++) {
+//            SchoolTypes level = AnalyzerUtils.getSchoolLevel(words.get(currentIndex));
+//            if (level == UNKNOWN_SCHOOL_LEVEL) {
+//                continue;
+//            }
+//
+//            List<String> targetData = words.subList(Math.max(usedIndex, currentIndex - 4), currentIndex + 1);
+//
+//            for (int i = 1; i < (targetData.size() + 1); i++) {
+//                List<String> rawData = targetData.subList(targetData.size() + Math.max(-(targetData.size()), -i), targetData.size());
+//
+//                SchoolToken schoolToken = new SchoolToken(rawData, locations);
+//                if (schoolToken.isSkip()) {
+//                    continue;
+//                }
+//
+//                String school = repository.findSchool(schoolToken);
+//
+//                if (school == null) {
+//                    continue;
+//                }
+//
+//                usedIndex = currentIndex;
+//
+//                schools.add(school);
+//            }
+//        }
+//
+//        return new ArrayList<>(schools);
+//    }
+
     private List<String> parse(String text) {
         List<String> locations = AnalyzerUtils.getLocationsFromText(text);
 
@@ -90,7 +140,7 @@ public class CommentsAnalyzerService {
         }
 
         int usedIndex = 0;
-        List<String> schools = new ArrayList<>();
+        List<String> schoolNameList = new ArrayList<>();
         for (int currentIndex = 0; currentIndex < words.size(); currentIndex++) {
             SchoolTypes level = AnalyzerUtils.getSchoolLevel(words.get(currentIndex));
             if (level == UNKNOWN_SCHOOL_LEVEL) {
@@ -107,7 +157,8 @@ public class CommentsAnalyzerService {
                     continue;
                 }
 
-                String school = repository.findSchool(schoolToken);
+
+                String school = findSchool(schoolToken);
 
                 if (school == null) {
                     continue;
@@ -115,11 +166,120 @@ public class CommentsAnalyzerService {
 
                 usedIndex = currentIndex;
 
-                schools.add(school);
+                schoolNameList.add(school);
             }
         }
 
-        return new ArrayList<>(schools);
+        return new ArrayList<>(schoolNameList);
     }
+
+    private String findSchool(SchoolToken schoolToken) {
+        List<Pattern> matchedPatterns = new ArrayList<>();
+
+
+        String levelPattern = AnalyzerUtils.getPatternFromLevel(schoolToken.getLevel());
+        List<String> kindPatterns = AnalyzerUtils.getPatternFromKinds(schoolToken.getKinds());
+
+
+        if (kindPatterns.size() > 0) {
+            matchedPatterns.add(Pattern.compile(String.format("(%s)+.*%s$",
+                    Pattern.quote(String.join("|", kindPatterns)),
+                    Pattern.quote(levelPattern))));
+        }
+
+        List<School> schools = repository.findBySchoolNameAndLevel(schoolToken.getSchoolName(), levelPattern)
+                .stream()
+                .filter(school -> {
+                    int matched = patternFindFromSchoolName(school.getName(), matchedPatterns);
+                    calculateScore(schoolToken, school);
+                    return matched == matchedPatterns.size();
+                })
+                .collect(Collectors.toList());
+
+        if (schools.size() == 0) {
+            return null;
+        }
+
+        schools.sort(new Comparator<School>() {
+            @Override
+            public int compare(School o1, School o2) {
+                Integer o1Value = o1.getScore();
+                Integer o2Value = o2.getScore();
+                Integer o1Len = o1.getName().length();
+                Integer o2Len = o2.getName().length();
+                if (o1Value.equals(o2Value)) {
+                    return o1Len.compareTo(o2Len);
+                } else {
+                    return o2Value.compareTo(o1Value);
+                }
+            }
+        });
+
+
+        return schools.get(0).getName();
+
+    }
+
+//    private List<School> calculateSchoolStatistics(List<School> schools, SchoolToken schoolToken, List<Pattern> matchedPatterns) {
+//        List<School> list = new ArrayList<>();
+//        for (School school : schools) {
+//            calculateScore(schoolToken, school);
+//
+//            int matched = patternFindFromSchoolName(school.getName(), matchedPatterns);
+//
+//            if (matched != matchedPatterns.size()) continue;
+//
+//
+//            list.add(school);
+//        }
+//
+//        return new ArrayList<>(list);
+//    }
+
+    private void calculateScore(SchoolToken schoolToken, School school) {
+        if (schoolToken.isGirlKind() && school.getGender().equals("녀")) {
+            school.plusScore();
+        }
+
+        if (!schoolToken.isGirlKind() && !school.getGender().equals("녀")) {
+            school.plusScore();
+        }
+
+
+        if (schoolToken.isSpecialKind() && Arrays.asList("특수목적고등학교", "특성화고등학교").contains(school.getProperty())) {
+            school.plusScore();
+        }
+
+        if (schoolToken.hasLocationIn(school.getName())) {
+            school.plusScore();
+            ;
+        }
+
+        if (schoolToken.hasLocationIn(school.getLocation())) {
+            school.plusScore();
+        }
+
+    }
+
+    private int patternFindFromSchoolName(String schoolName, List<Pattern> patterns) {
+        if (patterns.size() == 0) {
+            return 0;
+        }
+
+        int count = 0;
+
+        for (Pattern pattern : patterns) {
+            if (isMatch(pattern.matcher(schoolName))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isMatch(Matcher matcher) {
+        return matcher.find();
+    }
+
 
 }
